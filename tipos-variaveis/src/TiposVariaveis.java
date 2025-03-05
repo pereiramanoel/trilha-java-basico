0cd50ea8985f29ec96a51e12c4d6feb5f3a9e1e0
Manoel Pereira <manoelpereyra@outlook.com>
2025-03-05 11:07:30 -0300

    Segunda aula <Operadores>

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salarioMinimo = 1518;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        
+        System.out.println(numero);
+
+        final double VALORDEPI = 3.14;
+        
     }
 }
